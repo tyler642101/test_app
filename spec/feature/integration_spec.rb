1863666feef8279bdd8cd7720a98e5959843a0f8
Tyler Pomberg <tyler642101@tamu.edu>
2022-02-03 16:56:25 -0600

    fixed create book bug and added one integration test

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Some dude'
+    fill_in 'Price', with: '7.99'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
